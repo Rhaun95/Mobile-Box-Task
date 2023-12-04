cd52ae0b219a57fe10e5acd30adbac6808ea8136
Paul <derwort@imis.uni-luebeck.de>
2023-12-04 14:24:17 +0100

    ADD: InstructionPage content

--- mobile_box_task/lib/view/InstructionPage.dart
+++ mobile_box_task/lib/view/InstructionPage.dart
@@ -16,16 +16,42 @@ class InstructionPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Dies ist eine Anweisung.',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                children: [
+                  Text(
+                    'In the following you are going to go through the mobile Box Task. Please try to keep the blue box inside of the outer border and dont let it get smaller then the inner border, which is displayed in the web-interface. To achieve this, you can tilt your smartphone to the left or right to steer the box along the x-axis. By pushing the buttons in the bottom corners you can either accelerate or brake and make the box larger or smaller.',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Now please hold your smartphone horizontally to begin with the Mobile Box Task.',
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SizedBox(width: 350, height: 200, child: Placeholder()),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Button(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Driving(),
+                        ),
+                      );
+                    },
+                    text: "Continue",
+                  ),
+                ],
+              ),
             ),
-            Button(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const Driving()));
-                },
-                text: "Continue")
           ],
         ),
       ),
