258cbab5d2aac0d0bf5b47bb6aa784308fe98df7
Paul <derwort@imis.uni-luebeck.de>
2023-12-12 18:09:20 +0100

    ADD: websocket integration
    Co-authored-by: Haun <Rhaun95@users.noreply.github.com>
    Co-authored-by: Jan Heidinger <Heidiheidinger@users.noreply.github.com>
    Co-authored-by: Abdulrahman Doudaki <abdul_rahman_108643@svuonline.org>

--- server/app.js
+++ server/app.js
@@ -1,11 +1,54 @@
-var express = require('express') //Aufruf von express module
-var app = express() //Rückgabe von express in Variable speichern
-app.listen(3000, function() { //Server auf Port 3000 öffnen
-    console.log("express server on port 3000")
-})
+var express = require("express"); //Aufruf von express module
+var app = express(); //Rückgabe von express in Variable speichern
+var http = require("http");
+var server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', function(req,res) {
-    res.send(
-        "<h1>express server on port 3000</h1>"
-        )
-})
+var speed = 24;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  setInterval(() => {
+    socket.emit("new number", speed);
+  }, 100);
+
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+
+  socket.broadcast.emit("hi");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("gas button has been pressed", () => {
+    console.log("gas pressed");
+    speed += 1;
+    socket.emit("new number", speed);
+  });
+
+  socket.on("brake button pressed", () => {
+    console.log("brake pressed");
+    speed -= 1;
+    socket.emit("new number", speed);
+  });
+});
+
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+}); // This will emit the event to all connected sockets
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
